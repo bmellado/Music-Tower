f30d6d4f3552e2da263a29fbd6c6b5d040711cf2
Benjamín Mellado <38161980+bmellado@users.noreply.github.com>
2018-04-07 14:33:24 -0300

    Add files via upload

---
+++ readme.txt
@@ -0,0 +1,3 @@
+En la carpeta icy tower se encuentran los archivos .py, el archivo icy tower game es el que se tiene que ejecutar
+Se incluye ppt y pdf de la presentación
+El juego incluye dos sprites, una imagen jpg y un audio en mp3.
---
+++ texto.py
@@ -0,0 +1,12 @@
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+import pygame
+
+
+def draw_text(x, y, text, color=(0, 0, 0, 0), fondo=(255,255,255, 0), tamaño=90):
+    position = (x, y)
+    font = pygame.font.Font(None, tamaño)
+    text_surface = font.render(text, True, color, fondo)
+    text_data = pygame.image.tostring(text_surface, "RGBA", True)
+    glRasterPos2d(*position)
+    glDrawPixels(text_surface.get_width(), text_surface.get_height(), GL_RGBA, GL_UNSIGNED_BYTE, text_data)
